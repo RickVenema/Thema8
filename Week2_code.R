28589dc37adbe5b8b1b7b68ffddec8ecd9bdbd69
Rick Venema <kfvenema@bioinf.nl>
2018-05-08 11:36:52 +0200

    added model for week 2

--- Week2_code.R
+++ Week2_code.R
@@ -10,19 +10,44 @@ library(deSolve)
 #############
 # functions #
 #############
-parameters <- c(kt = 0.63, DR = 0, Kre = 0.57) 
+# parameters <- c(kd_Rm = 0.612, ks_Rm = 2.90, ks_r = 3.22, kd_R = 0.0572, D= 0, k_on= 0.00329) 
 
 
-model <- function(t, y, parms){
-  with(as.list(c(parms)), {
-    dDR.N_dt <- t * DR - Kre * y
-    return(list(c(dDR.N_dt)))
+# model <- function(t, y, parms){
+#   with(as.list(c(parms)), {
+#     dmRNA.R_dt <- ks_Rm * 1 - kd_Rm * mRNA.R
+#     dR_dt <- ks_r *  mRNA.R - k_on * D * R - kd_R * R
+#     return(list(c(dmRNA.R_dt, dR_dt)))
+#   }
+#   )
+# }
+
+
+parameters <- c(kd_Rm = 0.612, ks_Rm = 2.90, ks_r = 3.22, kd_R = 0.0572, D=100, k_on= 0.00329, IC50_Rm = 26.2,
+                k_t = 0.63, k_re = 0.57, Rf = 0.49) 
+
+
+
+volume <- function(t, y, parms){
+  with(as.list(c(y, parms)), {
+    dmRNA.R_dt <- ks_Rm * (1- (DR_N/ (IC50_Rm + DR_N)))- kd_Rm * mRNA.R 
+    dR_dt <- ks_r *  mRNA.R + Rf * k_re * DR_N - k_on * D * R - kd_R * R
+    dDR_dt <- k_on * D * R -k_t * DR
+    dDR_N_dt <- k_t * DR - k_re* DR_N
+    return(list(c(dmRNA.R_dt, dR_dt, dDR_N_dt, dDR_dt)))
   }
   )
 }
 
-state <- c(DR.N = 1)
-times <- seq(0.63, 2, by = 0.01)
 
-out <- ode(times = times, y = state, parms = parameters, func=model, method="euler")
+state <- c(mRNA.R = 4.74, R = 267, DR_N = 0, DR = 0)
+
+#define time sequence you want to run the model
+times <- seq(0, 48,  by = 1)
+# run simulation using continuous approach
+out  <- ode(times = times, y = state,   parms = parameters, func = volume, method = "euler")
+
+
 plot(out)
+
+
