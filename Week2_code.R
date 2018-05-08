cbc13bfa89ba85a27735e9a71e3b0c0ffcf78850
Rick Venema <kfvenema@bioinf.nl>
2018-05-08 18:14:57 +0200

    added documentation and started Assignment week 2 markdown file

--- Week2_code.R
+++ Week2_code.R
@@ -10,24 +10,13 @@ library(deSolve)
 #############
 # functions #
 #############
-# parameters <- c(kd_Rm = 0.612, ks_Rm = 2.90, ks_r = 3.22, kd_R = 0.0572, D= 0, k_on= 0.00329) 
 
-
-# model <- function(t, y, parms){
-#   with(as.list(c(parms)), {
-#     dmRNA.R_dt <- ks_Rm * 1 - kd_Rm * mRNA.R
-#     dR_dt <- ks_r *  mRNA.R - k_on * D * R - kd_R * R
-#     return(list(c(dmRNA.R_dt, dR_dt)))
-#   }
-#   )
-# }
-
-
+# Define the parameters
 parameters <- c(kd_Rm = 0.612, ks_Rm = 2.90, ks_r = 3.22, kd_R = 0.0572, D=53.409, k_on= 0.00329, IC50_Rm = 26.2,
                 k_t = 0.63, k_re = 0.57, Rf = 0.49) 
 
 
-
+# Define the model function
 volume <- function(t, y, parms){
   with(as.list(c(y, parms)), {
     dmRNA.R_dt <- ks_Rm * (1- (DR_N/ (IC50_Rm + DR_N)))- kd_Rm * mRNA.R 
@@ -39,15 +28,16 @@ volume <- function(t, y, parms){
   )
 }
 
-
+# Define the state
 state <- c(mRNA.R = 4.74, R = 267, DR_N = 0, DR = 0)
 
-#define time sequence you want to run the model
+# Define time sequence you want to run the model
 times <- seq(0, 48,  by = 1)
-# run simulation using continuous approach
+
+# Run simulation using continuous approach
 out  <- ode(times = times, y = state,   parms = parameters, func = volume, method = "euler")
 
-
+# Makes plots of the results of the model
 plot(out)
 
 
