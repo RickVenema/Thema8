ce3025df460ad2a751e15fffd5164515a3832c90
RickVenema <31951183+RickVenema@users.noreply.github.com>
2018-06-01 08:29:39 +0200

    started working on End Report

--- three-pool-model.R
+++ three-pool-model.R
@@ -25,15 +25,15 @@ parameters <- c(I_tzd = 0.69,
                 Q = 2,
                 temp_opt = 304.15, 
                 temp_z = 288,
-                k_FOC = 1.37,
+                k_FOC =1.37,
                 FOC_tzd = 0.5,
                 a1 = 0.001,
                 a2 = 0.0083,
                 b = 1,
-                HSOC1 = 0.1,
-                HSOC2 = 0.1)
+                HSOC1 = 1,
+                HSOC2 = 1)
 
-state <- c(dec_HSOC2_tzd <- 0.1, dec_HSOC1_tzd <- 0.1, dec_FOC_tzd <- 0)
+state <- c(dec_HSOC2_tzd <- 300, dec_HSOC1_tzd <- 300, dec_FOC_tzd <- 300)
 
 times <- seq(0, 20,  by = 1)
 
@@ -49,7 +49,7 @@ three.pool <- function(t, y, parms){
     
     dec_HSOC1_tzd <- -k_HSOC1 * dec_HSOC1_tzd * f_moist * f_temp
     dec_HSOC2_tzd <- -k_HSOC2 * dec_HSOC2_tzd * f_moist * f_temp
-    dec_FOC_tzd <- -k_FOC * FOC_tzd * f_clay * f_moist * f_temp
+    dec_FOC_tzd <- -k_FOC * dec_FOC_tzd * f_clay * f_moist * f_temp
     
     dFOC_tzd <- I_tzd + (dF_AD) + h * f2 * dec_HSOC1_tzd + h * dec_HSOC2_tzd - dec_FOC_tzd
     dHSOC1 <- (dF_AD) + h * f1 * dec_FOC_tzd - dec_HSOC1_tzd
@@ -66,7 +66,7 @@ out  <- ode(times = times, y = state,   parms = parameters, func = three.pool, m
 
 out <- as.data.frame(out)
 colnames(out)<- c("years", "FOC", "HSOC1", "HSOC2")
-plot(out$HSOC1 ~ out$years, type="l", main="Stuff; Red = HSOC2 \n Black = HSOC1 \n Blue = FOC", ylim=c(-2, 4))
+plot(out$HSOC1 ~ out$years, type="l", main="Stuff; Red = HSOC2 \n Black = HSOC1 \n Blue = FOC", ylim=c(0, 350))
 points(out$HSOC2 ~ out$years, type="l", col="red")
 points(out$FOC ~ out$years, type="l", col="blue")
 
