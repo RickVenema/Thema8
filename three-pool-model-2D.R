3bdcd36df689ef611c63776f203f93b6ee23fda3
RickVenema <31951183+RickVenema@users.noreply.github.com>
2018-06-01 12:59:22 +0200

    three pool model improvement
    We still don't get all the documentation, but we are getting there, we also had a mail from Remi, he needs some time to clean up the code, and then he will send it to us

---
+++ three-pool-model-2D.R
@@ -0,0 +1,37 @@
+library(deSolve)
+
+LVmod2D <- function (time, state, parms, N, Da, dx, dy){
+  P <- matrix(nr = N, nc = N, state[1:NN]) 
+  C <- matrix(nr = N, nc = N, state[-(1:NN)])
+  with (as.list(parms), {
+    dP <- rG * P *(1 - P/K) - rI * P *C  
+    dC <- rI * P * C * AE - rM * C
+    zero <- numeric(N)
+    ## Fluxes in x-direction; zero fluxes near boundaries 
+    FluxP <- rbind(zero, -Da * (P[-1,] - P[-N,])/dx, zero) 
+    FluxC <- rbind(zero, -Da * (C[-1,] - C[-N,])/dx, zero) 
+    dP <- dP - (FluxP[-1,] - FluxP[-(N+1),])/dx 
+    dC <- dC - (FluxC[-1,] - FluxC[-(N+1),])/dx  
+    ## Fluxes in y-direction
+    FluxP <- cbind(zero, -Da * (P[,-1] - P[,-N])/dy, zero) 
+    FluxC <- cbind(zero, -Da * (C[,-1] - C[,-N])/dy, zero) 
+    dP <- dP - (FluxP[,-1] - FluxP[,-(N+1)])/dy 
+    dC <- dC - (FluxC[,-1] - FluxC[,-(N+1)])/dy 
+    return(list(c(as.vector(dP), as.vector(dC)))) 
+  }) 
+}
+pars <- c(rI = 0.2, rG = 1.0, rM = 0.2, AE = 0.5, K = 10)
+R <- 20
+N <- 50
+dx <- R/N
+dy <- R/N
+Da <- 0.05
+NN <- N * N
+yini <- rep(0, 2 * N * N)
+cc <- c((NN/2):(NN/2+1) + N/2, (NN/2):(NN/2 + 1)- N/2)
+yini[cc] <- yini[NN + cc] <- 10
+times <- seq(0,200, by = 1)
+out <- ode.2D(y = yini, times = times, func = LVmod2D, 
+              parms = pars, dimens = c(N, N), N = N, 
+              dx = dx, dy = dy, Da = Da, ynames = FALSE, lrw = 440000)
+out[,2]
--- three-pool-model.R
+++ three-pool-model.R
@@ -10,12 +10,54 @@ library(deSolve)
 ##
 # dz = diepte
 ##
+three.pool <- function(t, y, parms){
+  with(as.list(c(parms)), {
+    dF_AD <- (A * C) + (-D * (C/z))
+    temp_z <- temp_z + z*0.1
+    f_clay <- 1 - 0.75 * Clay_z
+    f_temp <- Q ^((temp_z - temp_opt) /10)
+    
+    k_HSOC1 <- a1 * exp(-b * z)
+    k_HSOC2 <- a2 * exp(-b * z)    
+    
+    dec_HSOC1_tzd <- -k_HSOC1 * HSOC1 * f_moist * f_temp
+    dec_HSOC2_tzd <- -k_HSOC2 * HSOC2 * f_moist * f_temp
+    dec_FOC_tzd <- -k_FOC * FOC_tzd * f_clay * f_moist * f_temp
+    
+    dFOC_tzd <- I_tzd + (dF_AD) + h * f2 * dec_HSOC1_tzd + h * dec_HSOC2_tzd - dec_FOC_tzd
+    dHSOC1 <- (dF_AD) + h * f1 * dec_FOC_tzd - dec_HSOC1_tzd
+    dHSOC2 <- (dF_AD) + h * (1-f1) * dec_FOC_tzd + h * (1-f2) * dec_HSOC1_tzd - dec_HSOC2_tzd
+    
+    HSOC1 <- HSOC1 + dec_HSOC1_tzd
+    HSOC2 <- HSOC2 + dec_HSOC2_tzd
+    return(list(c(dFOC_tzd, dHSOC1, dHSOC2)))
+  }
+  )
+}
+
+
+
+ode.better <- function(){
+  array_z = c(0.1, 0.2, 0.3)
+  array_total = array(dim = c(100,4,3))
+  counter = 0
+  for(z in array_z){
+    print(z)
+    parameters['z'] = z
+    out <- ode(times = 1:100, y = state,   parms = parameters, func = three.pool, method = "euler")
+    out <- as.data.frame(out)
+    
+    counter = counter + 1
+  }
+  
+}
+print(array_total)
+
 
 parameters <- c(I_tzd = 0.69,
                 A = 21.6*(10^-4),
                 D = 9.64*(10^-4),
                 C = 2.29,
-                dz = 1, 
                 h = 0.34,
                 f2 = 0.8,
                 f1 = 0.86, 
@@ -26,47 +68,31 @@ parameters <- c(I_tzd = 0.69,
                 temp_opt = 304.15, 
                 temp_z = 288,
                 k_FOC =1.37,
-                FOC_tzd = 0.5,
+                FOC_tzd = 100,
                 a1 = 0.001,
                 a2 = 0.0083,
                 b = 1,
-                HSOC1 = 1,
-                HSOC2 = 1)
+                HSOC1 = 100,
+                HSOC2 = 100,
+                z = 0)
 
 state <- c(dec_HSOC2_tzd <- 300, dec_HSOC1_tzd <- 300, dec_FOC_tzd <- 300)
+species <- c()
 
+n <- 10
+dimensions <- c(4)
 times <- seq(0, 20,  by = 1)
 
-three.pool <- function(t, y, parms){
-  with(as.list(c(parms)), {
-    dF_AD <- (A * C) + (-D * (C/dz))
-    
-    f_clay <- 1 - 0.75 * Clay_z
-    f_temp <- Q ^((temp_z - temp_opt) /10)
-    
-    k_HSOC1 <- a1 * exp(-b * dz)
-    k_HSOC2 <- a2 * exp(-b * dz)    
-    
-    dec_HSOC1_tzd <- -k_HSOC1 * dec_HSOC1_tzd * f_moist * f_temp
-    dec_HSOC2_tzd <- -k_HSOC2 * dec_HSOC2_tzd * f_moist * f_temp
-    dec_FOC_tzd <- -k_FOC * dec_FOC_tzd * f_clay * f_moist * f_temp
-    
-    dFOC_tzd <- I_tzd + (dF_AD) + h * f2 * dec_HSOC1_tzd + h * dec_HSOC2_tzd - dec_FOC_tzd
-    dHSOC1 <- (dF_AD) + h * f1 * dec_FOC_tzd - dec_HSOC1_tzd
-    dHSOC2 <- (dF_AD) + h * (1-f1) * dec_FOC_tzd + h * (1-f2) * dec_HSOC1_tzd - dec_HSOC2_tzd
-    return(list(c(dFOC_tzd, dHSOC1, dHSOC2)))
-  }
-  )
-}
+ode.better()
 
 ####
 # not in attachment
 ####
-out  <- ode(times = times, y = state,   parms = parameters, func = three.pool, method = "euler")
 
-out <- as.data.frame(out)
 colnames(out)<- c("years", "FOC", "HSOC1", "HSOC2")
-plot(out$HSOC1 ~ out$years, type="l", main="Stuff; Red = HSOC2 \n Black = HSOC1 \n Blue = FOC", ylim=c(0, 350))
+plot(out$HSOC1 ~ out$years, type="l", main="Stuff; Red = HSOC2 \n Black = HSOC1 \n Blue = FOC", ylim=c(-150, 350))
 points(out$HSOC2 ~ out$years, type="l", col="red")
 points(out$FOC ~ out$years, type="l", col="blue")
+
+tmp <- out$FOC[5] - out$HSOC1[5] - out$HSOC2[5]
 
