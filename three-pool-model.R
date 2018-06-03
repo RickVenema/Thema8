fbce15461418254ea09cdc3214413e747be6e843
RickVenema <31951183+RickVenema@users.noreply.github.com>
2018-06-03 22:02:30 +0300

    weekend code jam
    worked all weekend on code / markdown and done a lot and I mean a lot of research online

--- three-pool-model.R
+++ three-pool-model.R
@@ -7,9 +7,9 @@
 ########
 library(deSolve)
 
-##
-# dz = diepte
-##
+#############
+# The model #
+#############
 three.pool <- function(t, y, parms){
   with(as.list(c(parms)), {
     dF_AD <- (A * C) + (-D * (C/z))
@@ -30,6 +30,7 @@ three.pool <- function(t, y, parms){
     
     HSOC1 <- HSOC1 + dec_HSOC1_tzd
     HSOC2 <- HSOC2 + dec_HSOC2_tzd
+    FOC_tzd <- dec_FOC_tzd + FOC_tzd
     return(list(c(dFOC_tzd, dHSOC1, dHSOC2)))
   }
   )
@@ -37,23 +38,10 @@ three.pool <- function(t, y, parms){
 
 
 
-ode.better <- function(){
-  array_z = c(0.1, 0.2, 0.3)
-  array_total = array(dim = c(100,4,3))
-  counter = 0
-  for(z in array_z){
-    print(z)
-    parameters['z'] = z
-    out <- ode(times = 1:100, y = state,   parms = parameters, func = three.pool, method = "euler")
-    out <- as.data.frame(out)
-    
-    counter = counter + 1
-  }
-  
-}
-print(array_total)
 
-
+##############
+# parameters #
+##############
 parameters <- c(I_tzd = 0.69,
                 A = 21.6*(10^-4),
                 D = 9.64*(10^-4),
@@ -74,25 +62,25 @@ parameters <- c(I_tzd = 0.69,
                 b = 1,
                 HSOC1 = 100,
                 HSOC2 = 100,
-                z = 0)
+                z = 0.1)
 
-state <- c(dec_HSOC2_tzd <- 300, dec_HSOC1_tzd <- 300, dec_FOC_tzd <- 300)
-species <- c()
+###################
+# Starting Values #
+###################
+state <- c(dec_HSOC2_tzd <- 0, dec_HSOC1_tzd <- 0, dec_FOC_tzd <- 0)
 
-n <- 10
-dimensions <- c(4)
+############
+# sequence #
+############
 times <- seq(0, 20,  by = 1)
 
-ode.better()
+out <- ode(times = times, y = state,   parms = parameters, func = three.pool, method = "euler")
+out <- as.data.frame(out)
 
-####
-# not in attachment
-####
 
 colnames(out)<- c("years", "FOC", "HSOC1", "HSOC2")
 plot(out$HSOC1 ~ out$years, type="l", main="Stuff; Red = HSOC2 \n Black = HSOC1 \n Blue = FOC", ylim=c(-150, 350))
 points(out$HSOC2 ~ out$years, type="l", col="red")
 points(out$FOC ~ out$years, type="l", col="blue")
 
-tmp <- out$FOC[5] - out$HSOC1[5] - out$HSOC2[5]
 
